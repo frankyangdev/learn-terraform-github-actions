837b4300824cbb149b60da6afc6aeb441ebb595c
frankyang <39177230+frankyangdev@users.noreply.github.com>
2021-02-08 11:50:45 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ provider "random" {}
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
+  ami                    = "ami-00068cd7555f543d5"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
